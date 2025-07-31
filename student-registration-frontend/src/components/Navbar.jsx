a9d71fd24fc0434c5848c4bb1d7f3d9da62d7d06
Matthew Tubbs <matthtubbs@gmail.com>
2025-07-30 19:02:07 -0600

    styled navbar

--- student-registration-frontend/src/components/Navbar.jsx
+++ student-registration-frontend/src/components/Navbar.jsx
@@ -2,9 +2,15 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/Courses">Courses</Link> | <Link to="/Students">Students</Link>
-    </nav>
+    <>
+        <div className='flex justify-between items-center p-2 h-20 bg-green-500'>
+            <h1>Student Registration the Website</h1>
+            <nav >
+            <Link to="/">Home</Link> | <Link to="/Courses">Courses</Link> | <Link to="/Students">Students</Link>
+            </nav>
+        </div>
+    </>
+
   );
 }
 
--- student-registration-frontend/src/pages/Main.jsx
+++ student-registration-frontend/src/pages/Main.jsx
@@ -1,8 +1,9 @@
 function Home() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Student Registration The Website</h1>
-            <h2>Welcome to Student Registration</h2>
+            <div>
+                <h1>Welcome to Student Registration</h1>
+            </div>
         </>
     )
 }
