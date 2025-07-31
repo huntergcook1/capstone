0cca71ef01517c95a4dcec3fa8908780f96693b1
Matthew Tubbs <matthtubbs@gmail.com>
2025-07-30 18:21:00 -0600

    moved some files around

---
+++ student-registration-frontend/src/pages/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Student Registration The Website</h1>
+      <h2>Welcome to Student Registration</h2>
+      <header>
+        <a href="">Courses</a>
+        <a href=""></a>
+      </header>
+    </>
+  )
+}
+
+export default App
